6692adc967e60e3695962cfab36b8b88302c05cb
MoonGi <01ki07@naver.com>
2024-03-28 15:10:30 +0900

    eslint 에러 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,41 @@ module.exports = {
     node: true,
   },
   extends: [
-    // 'plugin:vue/vue3-essential', // Lv1
-    'plugin:vue/vue3-strongly-recommended', // Lv2
-    // 'plugin:vue/vue3-recommended', // Lv3
-    "eslint:recommended",
+    "plugin:vue/vue3-essential", // Lv1
+    // "plugin:vue/vue3-strongly-recommended", // Lv2
+    // "plugin:vue/vue3-recommended", // Lv3
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    'vue/html-self-closing': ['error', {
-      html: {
-        void: 'always',
-        normal: 'never',
-        component: 'always'
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+          component: "always",
+        },
+        svg: "always",
+        math: "always",
       },
-      svg: 'always',
-      math: 'always'
-    }],
-    'vue/html-closing-bracket-newline': ['error', {
-      singleline: 'never',
-      multiline: 'never'
-    }],
-    "vue/multi-word-component-names": ["error", {
-      "ignores": ['Home', ]
-    }]
+    ],
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        singleline: "never",
+        multiline: "never",
+      },
+    ],
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: ["Home"],
+      },
+    ],
   },
 };
