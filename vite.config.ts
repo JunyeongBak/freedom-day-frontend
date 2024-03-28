c199d40d6f361c8d778187aaa8a0f26028f5af32
MoonGi <01ki07@naver.com>
2024-03-28 19:20:08 +0900

    배포 테스트 중

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path'; // path 모듈 임포트
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path"; // path 모듈 임포트
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/freedom-day/', // 애플리케이션의 베이스 URL 설정
+  // base: "/", // 애플리케이션의 베이스 URL 설정
   build: {
-    outDir: 'docs', // 빌드 출력 디렉토리를 'docs'로 설정
-    assetsDir: 'assets', // 정적 자산의 디렉토리 이름을 'assets'로 설정
+    outDir: "dist", // 빌드 출력 디렉토리를 'docs'로 설정
+    assetsDir: "assets", // 정적 자산의 디렉토리 이름을 'assets'로 설정
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // '@' 별칭을 'src' 디렉토리로 설정
+      "@": path.resolve(__dirname, "./src"), // '@' 별칭을 'src' 디렉토리로 설정
     },
   },
   server: {
