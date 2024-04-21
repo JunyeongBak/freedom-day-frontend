e41036dc6bfb4661ea138eee3400eb9455cb52a0
JunyeongBak <160200667+JunyeongBak@users.noreply.github.com>
2024-04-21 03:51:22 +0000

    signin init

---
+++ src/components/BarNavigationSignUp.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="nav-bar">
+      <van-icon name="arrow-left"/>
+      <span>사용자 정보 입력</span>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  
+  </script>
+  
+  <style lang="scss" scoped>
+    span{
+      font-size: 20px;
+      font-weight: 800;
+    }
+    div{
+      display: flex;
+      justify-content: left;
+      width: 360px;
+      height: 56px;
+      background-color: #fff;
+    }
+    .nav-bar{
+        align-items: center;
+    }
+  </style>
