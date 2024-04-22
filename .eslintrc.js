3833f0ca4da3a3e50120d3a932f835fc8ee1d043
MoonGi <01ki07@naver.com>
2024-04-22 17:34:58 +0900

    eslint 제거

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
-module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential", // Lv1
-    // "plugin:vue/vue3-strongly-recommended", // Lv2
-    // "plugin:vue/vue3-recommended", // Lv3
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-    requireConfigFile: false,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+// module.exports = {
+//   root: true,
+//   env: {
+//     node: true,
+//   },
+//   extends: [
+//     // "plugin:vue/vue3-essential", // Lv1
+//     // "plugin:vue/vue3-strongly-recommended", // Lv2
+//     // "plugin:vue/vue3-recommended", // Lv3
+//   ],
+//   parserOptions: {
+//     parser: "@babel/eslint-parser",
+//     requireConfigFile: false,
+//   },
+//   rules: {
+//     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+//     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+//   },
+// };
