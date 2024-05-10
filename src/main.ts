a4b0177a0fedec206fe2557c71057aeba749d21a
JunyeongBak <datarichjun@gmail.com>
2024-05-10 12:02:16 +0900

    피니아 store 테스트

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,12 @@ import './rem'; // rem 설정
 import Vant from 'vant'; // Vant UI 라이브러리
 import { Chart, registerables } from 'chart.js';
 import { createPinia } from 'pinia';
+import piniaPersistedState from 'pinia-persistedstate';
 Chart.register(...registerables);
 const pinia = createPinia();
+
+// pinia 플러그인으로 persisted state 플러그인 등록
+pinia.use(piniaPersistedState());
 
 // createApp 함수를 사용하여 Vue 애플리케이션 인스턴스를 생성
 createApp(App)
